6967e9e554780d096808f518b45150a513629da7
manucpivatto <manucp_@hotmail.com>
2023-03-06 15:25:04 -0300

    firs commit

---
+++ fundamentos-react-ts/src/App.tsx
@@ -0,0 +1,63 @@
+import { Header } from './components/Header';
+import { Post, PostType } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+import {  } from './components/Post'
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1606041008023-472dfb5e530f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+      name: 'Manoela Pivatto',
+      role: '@Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80',
+      name: 'Carla Soares',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id} //propriedade do react 
+                post={post}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App;
